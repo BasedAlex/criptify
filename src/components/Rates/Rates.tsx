6efb822444858e565b7af9e02ba0e5e49099c88d
BasedAlex <alexbroseph@mail.ru>
2023-06-28 16:51:54 +0400

    feat:pagination-fix

--- src/components/Rates/Rates.tsx
+++ src/components/Rates/Rates.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useGetAllAssetsQuery } from '../../store/fetchAPI/apiSlice'
 import Rate from './Rate/Rate'
 import { useDispatch, useSelector } from 'react-redux'
@@ -10,7 +10,7 @@ import { RootState } from '@/store/index'
 import { decreaseBalance } from '@/store/slices/balanceSlice'
 import Subheader from './Subheader/Subheader'
 import { DM_Mono } from 'next/font/google'
-import { TestPagination } from '../UI/Pagination/TestPagination'
+import { Pagination } from '../UI/Pagination/Pagination'
 
 const dmmono = DM_Mono({
   weight: ['400', '500'],
@@ -19,7 +19,6 @@ const dmmono = DM_Mono({
 })
 
 const Rates = () => {
-  console.log(useSelector((state) => state))
   const [openPort, setOpenPort] = useState(false)
   const [toast, setToast] = useState(false)
   const [message, setMessage] = useState<string>('')
@@ -28,6 +27,7 @@ const Rates = () => {
   const [offset, setOffset] = useState(0)
   const [limit, setLimit] = useState(100)
   const [currentPage, setCurrentPage] = useState('1')
+  const [skip, setSkip] = useState(false)
 
   const { data } = useGetAllAssetsQuery(
     { offset, limit },
@@ -80,7 +80,6 @@ const Rates = () => {
         <p>Volume(24h)</p>
         <p>Supply</p>
       </div>
-
       {openPort && (
         <Modal active={openPort} setActive={setOpenPort}>
           <div>
@@ -101,7 +100,6 @@ const Rates = () => {
           </div>
         </Modal>
       )}
-
       {toast && <Toaster toast={toast} setToast={setToast} message={message} />}
       <div className={`${dmmono.variable} text-xl shadow-lg`}>
         {data?.data?.map((item: any) => (
@@ -109,21 +107,15 @@ const Rates = () => {
         ))}
       </div>
 
-      {/* <Pagination limit={limit} offset={offset} setOffset={setOffset} /> */}
-      <TestPagination
+      <Pagination
         limit={limit}
         offset={offset}
         setOffset={setOffset}
         currentPage={currentPage}
+        skip={skip}
+        setSkip={setSkip}
         setCurrentPage={setCurrentPage}
       />
-      {/* <PaginationList
-        limit={limit}
-        offset={offset}
-        setOffset={setOffset}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      /> */}
     </div>
   )
 }
--- src/components/UI/Pagination/Pagination.tsx
+++ src/components/UI/Pagination/Pagination.tsx
@@ -1,45 +1,154 @@
+import { useEffect } from 'react'
+
 interface IProps {
   limit: number
   offset: number
   setOffset: (arg: number) => void
+  currentPage: string
+  setCurrentPage: (arg: string) => void
+  skip: boolean
+  setSkip: (arg: boolean) => void
 }
 
 export const Pagination = (props: IProps) => {
-  const { limit, offset, setOffset } = props
+  const {
+    limit,
+    offset,
+    setOffset,
+    currentPage,
+    setCurrentPage,
+    skip,
+    setSkip,
+  } = props
+
+  let actualPages: Array<string | number> = []
+
   const maxOffset = 2000
-  let pages: number[] = []
+  let totalPages: number[] = []
 
-  const paginate = (num = maxOffset / limit) => {
-    let i = 0
-    while (i < num) {
-      pages.push(i)
-      i++
+  let i = 1
+  while (i <= maxOffset / limit) {
+    totalPages.push(i)
+    i++
+  }
+
+  let isValid = totalPages.length > 7 && currentPage !== '...'
+
+  const paginate = () => {
+    if (isValid && +currentPage === 1) {
+      actualPages = totalPages.slice(0, 5)
+      actualPages = [
+        +currentPage,
+        +currentPage + 1,
+        +currentPage + 2,
+        +currentPage + 3,
+        '...',
+        totalPages.length,
+      ]
+      setCurrentPage(currentPage)
+    } else if (isValid && +currentPage > 1 && +currentPage === 2) {
+      actualPages = [
+        +currentPage - 1,
+        +currentPage,
+        +currentPage + 1,
+        +currentPage + 2,
+        '...',
+        totalPages.length,
+      ]
+      setCurrentPage(currentPage)
+    } else if (isValid && +currentPage > 1 && +currentPage === 3) {
+      actualPages = [
+        +currentPage - 2,
+        +currentPage - 1,
+        +currentPage,
+        +currentPage + 1,
+        '...',
+        totalPages.length,
+      ]
+      setCurrentPage(currentPage)
+    } else if (
+      isValid &&
+      +currentPage > 3 &&
+      +currentPage < totalPages.length - 2
+    ) {
+      actualPages = [
+        1,
+        '...',
+        +currentPage - 1,
+        +currentPage,
+        +currentPage + 1,
+        '...',
+        totalPages.length,
+      ]
+      setCurrentPage(currentPage)
+    } else if (
+      isValid &&
+      +currentPage > totalPages.length - 3 &&
+      +currentPage < totalPages.length - 1
+    ) {
+      actualPages = [
+        1,
+        '...',
+        +currentPage - 1,
+        +currentPage,
+        +currentPage + 1,
+        totalPages.length,
+      ]
+      setCurrentPage(currentPage)
+    } else if (isValid && +currentPage === totalPages.length) {
+      actualPages = [
+        1,
+        '...',
+        +currentPage - 3,
+        +currentPage - 2,
+        +currentPage - 1,
+        +currentPage,
+      ]
+      setCurrentPage(currentPage)
+    } else if (isValid && +currentPage === totalPages.length - 1) {
+      actualPages = [
+        1,
+        '...',
+        +currentPage - 2,
+        +currentPage - 1,
+        +currentPage,
+        +currentPage + 1,
+      ]
+      setCurrentPage(currentPage)
     }
-    return pages
+    return actualPages
   }
 
-  const handleLeft = () => {
-    if (offset > 0) {
-      setOffset(offset - limit)
+  const handlePaginate = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (e.currentTarget.value === 'prevPage') {
+      if (offset > 0) {
+        setOffset(offset - limit)
+        let nav = +currentPage - 1
+        setCurrentPage(nav.toString())
+      }
+    } else if (e.currentTarget.value === 'nextPage') {
+      if (offset <= maxOffset) {
+        setOffset(offset + limit)
+        let nav = +currentPage + 1
+        setCurrentPage(nav.toString())
+      }
+    } else if (e.currentTarget.value !== '...') {
+      setCurrentPage(e.currentTarget.value)
+      setOffset((+e.currentTarget.value - 1) * limit)
     }
   }
 
-  const handleRight = () => {
-    if (offset <= maxOffset) {
-      setOffset(offset + limit)
-    }
-  }
+  useEffect(() => {
+    setTimeout(() => setSkip(true), 3000)
+  }, [skip])
 
-  const handlePaginate = (e: React.MouseEvent<HTMLDivElement>) => {
-    setOffset(Number(e.currentTarget.innerText) * limit)
-  }
-
-  return (
+  return skip ? (
     <div className="container mx-auto">
       <div className="my-6 flex cursor-pointer items-center justify-center pt-5 text-gray-600 lg:mt-0">
-        <div
+        <button
           className="rounded px-4 py-2 hover:bg-gray-100"
-          onClick={handleLeft}
+          value="prevPage"
+          onClick={(e) => handlePaginate(e)}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -55,21 +164,29 @@ export const Pagination = (props: IProps) => {
               d="M15 19l-7-7 7-7"
             />
           </svg>
-        </div>
+        </button>
         <div className="flex gap-5 ">
-          {paginate().map((page: number) => (
-            <div
-              className="rounded px-4 py-2 hover:bg-gray-100"
-              key={page}
-              onClick={(e) => handlePaginate(e)}
+          {paginate().map((page: number | string, idx: number) => (
+            <button
+              className={
+                page === +currentPage
+                  ? 'select-none rounded px-4 py-2 text-blue-400 hover:bg-gray-100	'
+                  : 'select-none rounded px-4 py-2 hover:bg-gray-100'
+              }
+              key={idx}
+              value={page}
+              onClick={(e) => {
+                handlePaginate(e)
+              }}
             >
-              {page + 1}
-            </div>
+              {page}
+            </button>
           ))}
         </div>
-        <div
+        <button
           className="rounded px-4 py-2 hover:bg-gray-100"
-          onClick={handleRight}
+          value="nextPage"
+          onClick={(e) => handlePaginate(e)}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -85,8 +202,10 @@ export const Pagination = (props: IProps) => {
               d="M9 5l7 7-7 7"
             />
           </svg>
-        </div>
+        </button>
       </div>
     </div>
+  ) : (
+    <></>
   )
 }
